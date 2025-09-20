92a3bf09310f97c64c08e07f7b1ccda26eb381b3
ANON-n <thodibar2201@gmail.com>
2025-09-20 16:24:00 +0700

    khởi tạo backend

---
+++ backend/src/test/java/com/hotelhub/backend/HotelHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
