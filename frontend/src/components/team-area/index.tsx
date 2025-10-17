d899f07f9059b5952d7df92da7b81e8143f83cf7
minhkhanhh <102962225+minhkhanhh@users.noreply.github.com>
2025-10-18 01:19:40 +0700

    team

--- frontend/src/components/team-area/index.tsx
+++ frontend/src/components/team-area/index.tsx
@@ -8,7 +8,7 @@ import team1 from '../../assets/img/team/team01.jpg';
 import team2 from '../../assets/img/team/team02.jpg';
 import team3 from '../../assets/img/team/team03.jpg';
 import team4 from '../../assets/img/team/team04.jpg';
-import team5 from '../../assets/img/team/team05.jpg';
+
 import { Link } from "react-router-dom";
 
 const TeamArea = () => {
@@ -43,7 +43,7 @@ const TeamArea = () => {
     { img: team2, name: 'Trần Thị Lan', role: 'Quản lý lễ tân' },
     { img: team3, name: 'Lê Văn Hùng', role: 'Quản lý dịch vụ' },
     { img: team4, name: 'Phạm Thị Mai', role: 'Quản lý nhà hàng' },
-    { img: team5, name: 'Hoàng Văn Đức', role: 'Quản lý kỹ thuật' },
+    
   ];
 
   return (
